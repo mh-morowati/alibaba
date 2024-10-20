106f2709083cafe098e0babc1fd180002f4a6c30
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-10-20 11:16:17 +0330

    added useEffect for close when click
    added useEffect for close when click outSide

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import LogWithNumber from './LogWithNumber';
 import LogWithPassword from './LogWithPassword';
 
@@ -10,7 +10,8 @@ function Login() {
     const [loginPage, setLoginPage] = useState(false);
     const [number, setNumber] = useState(true);
     const [withPassword, setWithPassword] = useState(false);
-
+    const loginRef = useRef<HTMLDivElement | null>(null);
+    
     const onclick = () => {
         setLoginPage((prev) => !prev);
     };
@@ -24,9 +25,24 @@ function Login() {
         setWithPassword((prev) => !prev);
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (loginRef.current && !loginRef.current.contains(event.target as Node)) {
+                close();  // Close the login div if the click is outside
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (<>
         <button className="py-[1px] px-6" onClick={onclick}><PersonOutlineIcon /><span>ورود یا ثبت‌نام</span></button>
-        {loginPage && <div className='w-[600px] h-[500px] bg-white z-[9999999999] absolute mt-28 right-[450px] rounded-lg p-5'>
+        {loginPage && <div className='w-[600px] h-[500px] bg-white z-[9999999999] absolute mt-28 right-[450px] rounded-lg p-5'
+        ref={loginRef} >
             <button onClick={close} className='float-left text-3xl text-black'>X</button>
             {number && <div className='p-14 text-center'>
                 <LogWithNumber/>
